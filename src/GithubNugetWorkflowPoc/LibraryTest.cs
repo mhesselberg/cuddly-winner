47910a431ebce5d9fb1667479b7aac5881a88642
Hess <mbh@retus.dk>
2024-04-18 12:28:58 +0200

    will the prerelease version be bumped?

--- src/GithubNugetWorkflowPoc/LibraryTest.cs
+++ src/GithubNugetWorkflowPoc/LibraryTest.cs
@@ -2,9 +2,16 @@
 
 public class LibraryTest
 {
+    private string? HelloWorldString { get; set; }
     public string GetHelloWorldString()
     {
         // test that a new tag was added
-        return "Hello world";
+        return HelloWorldString ?? "Hello world";
+    }
+    
+    public void SetHelloWorldString(string helloWorldString)
+    {
+        // test that a new tag was added
+        HelloWorldString = helloWorldString;
     }
 }
